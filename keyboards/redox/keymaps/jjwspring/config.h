80ed8ea8c0d800b8f6ef1701e5936f297c71162d
Joel Williams <joel.williams@springboard.pro>
2022-10-07 18:00:37 +0100

    First Compile of jjwspring on rev1

--- keyboards/redox/keymaps/jjwspring/config.h
+++ keyboards/redox/keymaps/jjwspring/config.h
@@ -1,5 +1,21 @@
 #pragma once
 
+/* Use I2C or Serial, not both */
+#define USE_SERIAL
+// #define USE_I2C
+
+/* Select hand configuration */
+#define MASTER_LEFT
+// #define MASTER_RIGHT
+// #define EE_HANDS
+
+#undef RGBLED_NUM
+#define RGBLIGHT_ANIMATIONS
+#define RGBLED_NUM 14
+#define RGBLIGHT_HUE_STEP 8
+#define RGBLIGHT_SAT_STEP 8
+#define RGBLIGHT_VAL_STEP 8
+
 // default but used in macros
 #undef TAPPING_TERM
 #define TAPPING_TERM 200
