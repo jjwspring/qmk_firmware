eda853ea7a0004d84c7a96ff68a395c92df5bf86
Joel Williams <joel.williams@springboard.pro>
2023-01-14 22:21:49 +0000

    Add EE_HANDS and USE_SERIAL to 18 key config.h

--- keyboards/redox/keymaps/18_keys/config.h
+++ keyboards/redox/keymaps/18_keys/config.h
@@ -1,5 +1,14 @@
 #pragma once
 
+/* Use I2C or Serial, not both */
+#define USE_SERIAL
+// #define USE_I2C
+
+/* Select hand configuration */
+//#define MASTER_LEFT
+//#define MASTER_RIGHT
+#define EE_HANDS
+
 // default but used in macros
 #undef TAPPING_TERM
 #define TAPPING_TERM 200
